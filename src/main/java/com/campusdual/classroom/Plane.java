da51d7b897be86dc0818f76e004122f103ece4b9
eluiui <jesusmiguez@gmail.com>
2024-09-25 14:17:49 +0200

    completado

--- src/main/java/com/campusdual/classroom/Plane.java
+++ src/main/java/com/campusdual/classroom/Plane.java
@@ -1,6 +1,6 @@
 package com.campusdual.classroom;
 
-public class Plane{
+public class Plane implements  IMachine{
 
 	private final String name;
 
@@ -18,5 +18,22 @@ public class Plane{
 
 	public void fly() {
 		System.out.println("El avión está volando");
+	}
+
+	@Override
+	public void start() {
+		System.out.println(name + " esta encendido");
+		takeOff();
+	}
+
+	@Override
+	public void stop() {
+		System.out.println(name + " esta apagado");
+		land();
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println(name + " en mantenimiento");
 	}
 }
